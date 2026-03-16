9481cd6e0fc3b6dfe8060ed9c8cc2caead0d236c
Dash1Nova <zdanovicdarja@gmail.com>
2026-03-16 21:41:42 +0200

    trying to adjust for different containers 3.0

--- src/main.cpp
+++ src/main.cpp
@@ -11,48 +11,48 @@ int main() {
 
     int meniu = inputInt(
         "Koki konteineri naudoti:\n"
-        "1 - vector"
-        "2 - list"
-        "3 - deque", 1, 3);
+        "1 - vector\n"
+        "2 - list\n"
+        "3 - deque\n", 1, 3);
     
     if (meniu == 1) {
         std::vector<Student> Students;
         while (true) {
-        int choice = showMeniu();
+            int choice = showMeniu();
             
-        if (choice == 1) manualInput(Students), handleOutput(Students);
-        else if (choice == 2) generateGrades(Students), handleOutput(Students);
-        else if (choice == 3) generateNamesGrades(Students), handleOutput(Students);  
-        else if (choice == 4) scanFile(Students);
-        else if (choice == 5) generateFile();
-        else if (choice == 6) sortingStudents();
-        else if (choice == 7) break;
+            if (choice == 1) manualInput(Students), handleOutput(Students);
+            else if (choice == 2) generateGrades(Students), handleOutput(Students);
+            else if (choice == 3) generateNamesGrades(Students), handleOutput(Students);  
+            else if (choice == 4) scanFile(Students);
+            else if (choice == 5) generateFile();
+            else if (choice == 6) sortingStudents(Students);
+            else if (choice == 7) break;
         }
     } else if (meniu == 2) {
         std::list<Student> Students;
         while (true) {
-        int choice = showMeniu();
+            int choice = showMeniu();
             
-        if (choice == 1) manualInput(Students), handleOutput(Students);
-        else if (choice == 2) generateGrades(Students), handleOutput(Students);
-        else if (choice == 3) generateNamesGrades(Students), handleOutput(Students);  
-        else if (choice == 4) scanFile(Students);
-        else if (choice == 5) generateFile();
-        else if (choice == 6) sortingStudents();
-        else if (choice == 7) break;
+            if (choice == 1) manualInput(Students), handleOutput(Students);
+            else if (choice == 2) generateGrades(Students), handleOutput(Students);
+            else if (choice == 3) generateNamesGrades(Students), handleOutput(Students);  
+            else if (choice == 4) scanFile(Students);
+            else if (choice == 5) generateFile();
+            else if (choice == 6) sortingStudents(Students);
+            else if (choice == 7) break;
         }
     } else if (meniu == 3) {
         std::deque<Student> Students;
         while (true) {
-        int choice = showMeniu();
+            int choice = showMeniu();
             
-        if (choice == 1) manualInput(Students), handleOutput(Students);
-        else if (choice == 2) generateGrades(Students), handleOutput(Students);
-        else if (choice == 3) generateNamesGrades(Students), handleOutput(Students);  
-        else if (choice == 4) scanFile(Students);
-        else if (choice == 5) generateFile();
-        else if (choice == 6) sortingStudents();
-        else if (choice == 7) break;
+            if (choice == 1) manualInput(Students), handleOutput(Students);
+            else if (choice == 2) generateGrades(Students), handleOutput(Students);
+            else if (choice == 3) generateNamesGrades(Students), handleOutput(Students);  
+            else if (choice == 4) scanFile(Students);
+            else if (choice == 5) generateFile();
+            else if (choice == 6) sortingStudents(Students);
+            else if (choice == 7) break;
         }
     }
     return 0;
