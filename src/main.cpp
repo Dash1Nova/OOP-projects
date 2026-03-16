a24116b0a5249b658b19afdb426a80e806c31316
Dash1Nova <zdanovicdarja@gmail.com>
2026-03-16 23:22:02 +0200

    execution duration

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include <ctime>
 #include <list>
 #include <deque>
+#include <chrono>
+#include <iostream>
 
 int main() {
     srand(time(NULL));
@@ -14,12 +16,15 @@ int main() {
         "1 - vector\n"
         "2 - list\n"
         "3 - deque\n", 1, 3);
-    
-    if (meniu == 1) {
-        std::vector<Student> Students;
-        while (true) {
+
+        
+        if (meniu == 1) {
+            std::vector<Student> Students;
+            while (true) {
             int choice = showMeniu();
-            
+                
+            auto full1Start = std::chrono::high_resolution_clock::now();
+
             if (choice == 1) manualInput(Students), handleOutput(Students);
             else if (choice == 2) generateGrades(Students), handleOutput(Students);
             else if (choice == 3) generateNamesGrades(Students), handleOutput(Students);  
@@ -27,12 +32,16 @@ int main() {
             else if (choice == 5) generateFile();
             else if (choice == 6) sortingStudents(Students);
             else if (choice == 7) break;
+            auto full1End = std::chrono::high_resolution_clock::now();
+            std::cout << "Visos programos vykdymo laikas (s): " << std::chrono::duration<double>(full1End - full1Start).count() << "\n";
         }
     } else if (meniu == 2) {
         std::list<Student> Students;
         while (true) {
             int choice = showMeniu();
             
+            auto full2Start = std::chrono::high_resolution_clock::now();
+
             if (choice == 1) manualInput(Students), handleOutput(Students);
             else if (choice == 2) generateGrades(Students), handleOutput(Students);
             else if (choice == 3) generateNamesGrades(Students), handleOutput(Students);  
@@ -40,12 +49,17 @@ int main() {
             else if (choice == 5) generateFile();
             else if (choice == 6) sortingStudents(Students);
             else if (choice == 7) break;
+
+            auto full2End = std::chrono::high_resolution_clock::now();
+            std::cout << "Visos programos vykdymo laikas (s): " << std::chrono::duration<double>(full2End - full2Start).count() << "\n";
         }
     } else if (meniu == 3) {
         std::deque<Student> Students;
         while (true) {
             int choice = showMeniu();
             
+            auto full3Start = std::chrono::high_resolution_clock::now();
+
             if (choice == 1) manualInput(Students), handleOutput(Students);
             else if (choice == 2) generateGrades(Students), handleOutput(Students);
             else if (choice == 3) generateNamesGrades(Students), handleOutput(Students);  
@@ -53,6 +67,9 @@ int main() {
             else if (choice == 5) generateFile();
             else if (choice == 6) sortingStudents(Students);
             else if (choice == 7) break;
+
+            auto full3End = std::chrono::high_resolution_clock::now();
+            std::cout << "Visos programos vykdymo laikas (s): " << std::chrono::duration<double>(full3End - full3Start).count() << "\n";
         }
     }
     return 0;
